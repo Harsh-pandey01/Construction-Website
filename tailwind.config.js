69bd16e66e0c438d9b05de65d72fe595e5b81457
Harsh-Pandey <harshcse7571@gmail.com>
2024-10-29 11:33:07 -0700

    Done About Section

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      backgroundImages: {
+        heroImage:
+          "url(https://static.wixstatic.com/media/b31d0e84cb714761b2b1f06c305023c7.jpg/v1/fill/w_300,h_225,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/b31d0e84cb714761b2b1f06c305023c7.jpg)",
+      },
+    },
+  },
+  plugins: [],
+};
